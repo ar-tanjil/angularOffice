69703c58ce8c1cd5c3cf26a95906046e17fcb700
ar-tanjil <ashiq.edu.bd@gmail.com>
2024-03-03 12:46:50 +0600

    refinded department

--- src/app/core/departments/department-form/department-form.component.ts
+++ src/app/core/departments/department-form/department-form.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { ActivatedRoute } from '@angular/router';
+import { DepDatasource } from 'src/app/model/department/dep.datasource';
 import { Department } from 'src/app/model/department/deparment';
 import { DepModel } from 'src/app/model/department/department.model';
 
@@ -14,36 +16,46 @@ export class DepartmentFormComponent {
   title: string = ""
   department: Department = new Department();
   editing: boolean = false;
-  constructor(private model: DepModel, private route: ActivatedRoute) {
+  constructor(
+    private depData: DepDatasource,
+    private route: ActivatedRoute,
+    @Inject(MAT_DIALOG_DATA) public data: { id: number },
+    public dialogRef: MatDialogRef<DepartmentFormComponent>
+  ) {
 
-    route.params.subscribe(params => {
-      this.editing = params["mode"] == 'edit';
-      let id = params["id"];
-      if (id) {
-        this.model.getOrgDepartment(id).subscribe(dep => {
+      if (this.data.id) {
+        this.depData.getById(this.data.id).subscribe(dep => {
           this.department = dep;
           this.departmentForm.patchValue(this.department);
         })
       }
-    })
-
-
-
   }
 
 
   departmentForm: FormGroup = new FormGroup({
     id: new FormControl(),
-    departmentName: new FormControl(),
+    departmentName: new FormControl("", Validators.required),
     managerId: new FormControl(),
     mangagerName: new FormControl(),
     departmentDesc: new FormControl()
   })
 
   submitForm() {
-    Object.assign(this.department, this.departmentForm.value);
-    this.model.saveDepartment(this.department);
-    this.departmentForm.reset();
+    if (this.departmentForm.valid) {
+      Object.assign(this.department, this.departmentForm.value);
+      this.departmentForm.reset();
+      if (this.department.id) {
+        this.depData.update(this.department).subscribe(dep => {
+          this.dialogRef.close(dep);
+
+        })
+      } else {
+        this.depData.save(this.department).subscribe(dep => {
+          this.dialogRef.close(dep);
+        })
+      }
+
+    }
   }
 
 }
--- src/app/core/departments/departments.component.ts
+++ src/app/core/departments/departments.component.ts
@@ -5,6 +5,9 @@ import { DepModel } from 'src/app/model/department/department.model';
 import { FormMessage, FormOwner } from 'src/app/model/from.message.service';
 import { DepartmentFormComponent } from './department-form/department-form.component';
 import { auto } from '@popperjs/core';
+import { ReplaySubject } from 'rxjs';
+import { DepDatasource } from 'src/app/model/department/dep.datasource';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-departments',
@@ -13,25 +16,132 @@ import { auto } from '@popperjs/core';
 })
 export class DepartmentsComponent {
 
-    constructor(private model: DepModel, private dialog: MatDialog){
-
-    }
-
-    getDepartmetns():Department[]{
-      return this.model.getDepartments();
-    }
 
 
-    openDialog() {
-      let addSalaryDialog = this.dialog.open(DepartmentFormComponent, {
-        height: auto,
-        width: '40%'
-      }
-      );
-      addSalaryDialog.afterClosed().subscribe(ob => {
-        
-      })
+  departments: Department[];
+  private locator = (employee: Department, id?: number) => employee.id == id;
+  private replaySubject: ReplaySubject<Department[]>;
+
+
+  constructor(private depData: DepDatasource, private dialog: MatDialog) {
+    this.departments = new Array<Department>();
+    this.replaySubject = new ReplaySubject<Department[]>(1);
+    this.getDepartments();
+
+
   }
+
+  getDepartments() {
+    this.depData.getAll().subscribe(emp => {
+      this.departments = emp;
+      this.replaySubject.next(emp);
+      this.replaySubject.complete();
+    })
+  }
+
+
+  openDialog() {
+    let addSalaryDialog = this.dialog.open(DepartmentFormComponent, {
+      height: auto,
+      width: '40%',
+      data: {
+        id: null
+      }
+    }
+    );
+    addSalaryDialog.afterClosed().subscribe(ob => {
+      if (ob) {
+        let index = this.departments.push(ob);
+      }
+
+    })
+  }
+
+
+
+  updateDialog(id: number) {
+
+    if (id < 0) {
+      return;
+    }
+
+    let addSalaryDialog = this.dialog.open(DepartmentFormComponent, {
+      height: auto,
+      width: '40%',
+      data: {
+        id: id
+      }
+    }
+    );
+    addSalaryDialog.afterClosed().subscribe(ob => {
+      if (ob) {
+        let index = this.departments.findIndex(dep => this.locator(dep, ob.id));
+        this.departments.splice(index, 1, ob);
+        Swal.fire('Updated succesfully!', 'success')
+
+      }
+    })
+  }
+
+
+
+  delete(id: number) {
+
+    if (id < 0) {
+      return;
+    }
+
+    Swal.fire({
+
+      title: 'Are you sure want to remove?',
+
+      icon: 'warning',
+
+      showCancelButton: true,
+
+      confirmButtonText: 'Yes, delete it!',
+
+      cancelButtonText: 'No, keep it'
+
+    }).then((result) => {
+
+      if (result.value) {
+
+        this.depData.delete(id).subscribe(dep => {
+          let index = this.departments.findIndex(item => this.locator(item, id));
+          this.departments.splice(index, 1);
+
+          Swal.fire(
+
+            'Deleted!',
+            'success'
+
+          )
+        })
+
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+
+        Swal.fire(
+
+          'Cancelled',
+
+          'error'
+
+        )
+      }
+
+    })
+
+  }
+
+
+
+
+
+
+
+
+
 
 
 }
