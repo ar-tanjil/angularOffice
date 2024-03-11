5d6f190d9a4d987b3971c6f286ce9d79f851be77
ar-tanjil <ashiq.edu.bd@gmail.com>
2024-03-12 04:51:59 +0600

    1st shehri done

--- src/app/core/dashbord/emp-dep-chart/emp-dep-chart.component.ts
+++ src/app/core/dashbord/emp-dep-chart/emp-dep-chart.component.ts
@@ -14,50 +14,62 @@ export class EmpDepChartComponent implements OnInit {
 
   departmentChart: DepartmentChart[] = new Array<DepartmentChart>();
   replaySubject: ReplaySubject<DepartmentChart[]>;
-
+  chartOptions: any = [];
+  
 
   constructor(
     private depData: DepartmentDatasource,
   ) {
       this.departmentChart = new Array<DepartmentChart>();
       this.replaySubject = new ReplaySubject<DepartmentChart[]>(1);
+
   }
 
 
   ngOnInit(): void {
+this.getChartData();
+ 
+  }
+
+
+
+    getChartData(){
+      this.depData.getChartData().subscribe(data => {
+        this.departmentChart = data;
+        this.replaySubject.next(data);
+        this.replaySubject.complete();
+
+
+        this.chartOptions = {
+          animationEnabled: true,
+          title: {
+            text: "Employee By Department"
+          },
+          data: [{
+            type: "pie",
+            startAngle: -90,
+            indexLabel: "{name}: {y}",
+            yValueFormatString: "#,###.##'%'",
+            dataPoints: this.departmentChart
+          }]}
+
+      })
+    }
+ 
+
+
 
 
   }
 
 
-  getChartData(){
-    this.depData.getChartData().subscribe(data => {
-      this.departmentChart = data;
-      this.replaySubject.next(data);
-      this.replaySubject.complete();
-    })
-  }
 
-
-
-  chartOptions = {
-    animationEnabled: true,
-    title: {
-      text: "Employee By Department"
-    },
-    data: [{
-      type: "pie",
-      startAngle: -90,
-      indexLabel: "{name}: {y}",
-      yValueFormatString: "#,###.##'%'",
-      dataPoints: this.departmentChart
-    }]
-  }
+ 
 
 
 
 
-}
+
 
 // https://canvasjs.com/angular-charts/pie-chart-index-data-label/
 // https://code-projects.org/hr-management-system-in-java-with-source-code/
